1035f6e853668ebe5352e836e20b4df12564325c
Joefrey Klein Bayan <joefreykleinbayan@gmail.com>
2022-10-13 22:00:05 +0100

    included centroid for pedestrians

--- trial.py
+++ trial.py
@@ -24,11 +24,34 @@ with open('/Users/joefreybayan/Desktop/ha_challenge/resources/video_1_detections
 #             temp2.append(x)
 
 # print (temp2)
+# def calculate_centroid(*bounding_box):
 
-for y in range (1,6):
-    z = str(y)
-    x = dict1[z]["bounding boxes"]
-    a = dict1[z]
-    print (a["detected classes"])
+#     centroid = (bounding_box[0] + (bounding_box[2]//2) , bounding_box[1] + (bounding_box[3]//2))
 
+#     return centroid
 
+# for y in range (1,6):
+#     z = str(y)
+#     x = dict1[z]["bounding boxes"]
+#     a = dict1[z]["detected classes"]
+#     for box in x:
+#         b = calculate_centroid(x[box])
+#         print (b)
+
+# class Person:
+#   def __init__(mysillyobject, name, age):
+#     mysillyobject.name = name
+#     mysillyobject.age = age
+
+#   def myfunc(abc):
+#     print("Hello my name is " + abc.name)
+
+# p1 = Person("John", 36)
+# p1.myfunc()
+
+# x = {}
+# for y in range (3):
+#     x[y] = (1, 1)
+#     for z in range (2):
+#         x[z].update(2,2)
+# print(x)
